1144672be811b2c46c7be26a129b9efe2d1aa4c6
tomtomgo92 <tmillet@thatmuch.fr>
2026-04-27 08:54:47 +0200

    test: mock IntersectionObserver in setupTests.ts to fix CI

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,13 @@
 import '@testing-library/jest-dom';
+
+class IntersectionObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+Object.defineProperty(window, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver,
+});
